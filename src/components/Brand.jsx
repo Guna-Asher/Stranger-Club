bdb5524389a7be153b2f61ea6f64e56b42c67ac5
Guna-Asher <guna734941@gmail.com>
2026-09-06 10:34:52 +0530

    fix: Correct logo file name casing in HTML and Brand component

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,4 +1,4 @@
 import { cn } from '../lib/format';
 import { DEFAULT_MATCH } from '../lib/constants';
 
-export default function Brand({ light = false }) { return <a className={cn('brand', light && 'brand--light')} href={`/m/${DEFAULT_MATCH}`}><img className="brand-mark" src="/logo.png" alt="Stranger Club" />STRANGER CLUB</a>; }
+export default function Brand({ light = false }) { return <a className={cn('brand', light && 'brand--light')} href={`/m/${DEFAULT_MATCH}`}><img className="brand-mark" src="/Logo.png" alt="Stranger Club" />STRANGER CLUB</a>; }
